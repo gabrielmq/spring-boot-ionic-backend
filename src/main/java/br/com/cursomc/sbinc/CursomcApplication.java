6ccc40f37ec47de37bb61071cf290beb0c82283a
gabriel <gabrielmqs04@gmail.com>
2018-09-29 15:08:04 -0300

    aula 18 - implementando Estado e Cidade
    atualizando a classe CursomcApplication para carregar no db Estado e Cidade
    ajustando construtores das classes Estado/Cidade
    adicionando implementação dda classe CidadeRepository
    adicionando implementação dda classe EstadoRepository
    adicionando implementação da classe Estado
    adicionando implementação da classe Cidade

--- src/main/java/br/com/cursomc/sbinc/CursomcApplication.java
+++ src/main/java/br/com/cursomc/sbinc/CursomcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.cursomc.sbinc.domain.Categoria;
+import br.com.cursomc.sbinc.domain.Cidade;
+import br.com.cursomc.sbinc.domain.Estado;
 import br.com.cursomc.sbinc.domain.Produto;
 import br.com.cursomc.sbinc.repositories.CategoriaRepository;
+import br.com.cursomc.sbinc.repositories.CidadeRepository;
+import br.com.cursomc.sbinc.repositories.EstadoRepository;
 import br.com.cursomc.sbinc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -21,12 +25,23 @@ public class CursomcApplication implements CommandLineRunner {
 	@Autowired
 	private ProdutoRepository produtoRepository;
 	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
+		criarProdutoCategoria();
+		criarEstadoCidade();
+	}
+
+	private void criarProdutoCategoria() {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
@@ -44,4 +59,20 @@ public class CursomcApplication implements CommandLineRunner {
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
+	
+	private void criarEstadoCidade() {
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
+ 		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+	}
+
 }
