16f2af8fc59eee9bda78e1dad09c7711544742a0
gabrielmq <gabrielmqs04@gmail.com>
2018-10-03 12:09:03 -0300

    aula 44 - implementando validacao customizada para nao permitir email repetido no update de cliente
    adicionando anotacao @ClienteUpdate no dto ClienteDTO
    implementando validator ClienteUpdateValidator da anotacao @ClienteUpdate
    implementando anotacao personalizada @ClienteUpdate para validacoes

---
+++ src/main/java/br/com/cursomc/sbinc/services/validation/ClienteUpdateValidator.java
@@ -0,0 +1,51 @@
+package br.com.cursomc.sbinc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import br.com.cursomc.sbinc.domain.Cliente;
+import br.com.cursomc.sbinc.domain.dto.ClienteDTO;
+import br.com.cursomc.sbinc.repositories.ClienteRepository;
+import br.com.cursomc.sbinc.resources.exceptions.FieldMessage;
+
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private HttpServletRequest request;
+	
+	@Override
+	public void initialize(ClienteUpdate ann) { }
+	
+	@Override
+	public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
+		List<FieldMessage> list = new ArrayList<>();
+		
+		@SuppressWarnings("unchecked") // pega um map de variaveis de URI do request, no caso vai ser o id do cliente
+		Map<String, String> requestParams = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		
+		Integer uriId = Integer.parseInt(requestParams.get("clienteId"));
+		
+		Cliente aux = clienteRepository.findByEmail(objDto.getEmail());
+		if (aux != null && !aux.getId().equals(uriId)) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
+		
+		list.forEach(e -> {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName()).addConstraintViolation();
+		});
+		
+		return list.isEmpty();
+	}
+}
