398dfce66dfd670ad5b9c5a606761be7113b6925
gabriel <gabrielmqs04@gmail.com>
2018-10-17 00:03:36 -0300

    aula 70 - implementando autorização
    atualizando a classe SecurityConfig
    atualizando JWTUtils
    implementando filtro de autorização JWTAuthorizationFilter

--- src/main/java/br/com/cursomc/sbinc/config/SecurityConfig.java
+++ src/main/java/br/com/cursomc/sbinc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import br.com.cursomc.sbinc.security.JWTAuthenticationFilter;
+import br.com.cursomc.sbinc.security.JWTAuthorizationFilter;
 import br.com.cursomc.sbinc.security.JWTUtils;
 
 @Configuration
@@ -49,6 +50,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
