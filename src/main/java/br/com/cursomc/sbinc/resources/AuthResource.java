2f4c2d6b17f1bc0c2eb873d25f2648f48e9e0786
gabriel <gabrielmqs04@gmail.com>
2018-10-19 23:33:09 -0300

    aula 75 - implementando endpoint que gera uma nova senha
    implementando AuthService
    implementando dto EmailDTO
    atualizando classe AbstractEmailService
    atualizando interface EmailService
    atualizando AuthResource implementando endpoint forgot
    atualizando SecurityConfig liberando acesso ao endpoint forgot

--- src/main/java/br/com/cursomc/sbinc/resources/AuthResource.java
+++ src/main/java/br/com/cursomc/sbinc/resources/AuthResource.java
@@ -1,15 +1,19 @@
 package br.com.cursomc.sbinc.resources;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.cursomc.sbinc.domain.dto.EmailDTO;
 import br.com.cursomc.sbinc.security.JWTUtils;
 import br.com.cursomc.sbinc.security.UserSS;
+import br.com.cursomc.sbinc.services.AuthService;
 import br.com.cursomc.sbinc.services.UserService;
 
 @RestController
@@ -19,11 +23,20 @@ public class AuthResource {
 	@Autowired
 	private JWTUtils jwtUtil;
 	
+	@Autowired
+	private AuthService service;
+	
 	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST) 
 	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
 		UserSS user = UserService.authenticated();
 		String token = jwtUtil.generateToken(user.getUsername());
 		response.addHeader("Authorization", "Bearer "+token);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/forgot", method = RequestMethod.POST) 
+	public ResponseEntity<Void> forgot(@Valid @RequestBody EmailDTO emailDto) {
+		service.sendNewPassword(emailDto.getEmail());
 		return ResponseEntity.noContent().build();
 	}
 }
